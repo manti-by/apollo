8baf9aa8abf3980528703456093769175dde3d57
Alexander Chaika <manti.by@gmail.com>
2015-08-29 21:18:50 +0000

    Add term sensors support

--- worker/config.py
+++ worker/config.py
@@ -1,5 +1,12 @@
 import os
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
+DEBUG = True
+HOST = '0.0.0.0'
+
+TERM_INPUT = 7
+TERM_01 = '000007173569'
+TERM_02 = '000007177269'
+
 SQLALCHEMY_DATABASE_URI = 'mysql://pibot:P1B0t@192.168.1.2/pibot'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(ROOT_PATH, '../database')
--- worker/run.py
+++ worker/run.py
@@ -1,15 +1,49 @@
-from flask import Flask, json, request
-from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+import RPi.GPIO as GPIO
+from flask import Flask, jsonify, request
+
+from utils import get_onewire_value
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
 
 @app.route("/")
 def index():
-    return json.dumps({ 'result': 200 })
+    try:
+        result = {
+            'status'    : 200,
+            'result'    : {
+                'term_01' : get_onewire_value(app.config['TERM_01']),
+                'term_02' : get_onewire_value(app.config['TERM_02'])
+            }
+        }
+    except Exception as e:
+        result = {
+            'status'    : 500,
+            'message'   : e.message
+        }
+    return jsonify(result)
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        return jsonify({
+            'status'    : 500,
+            'message'   : 'Not running with the Werkzeug Server'
+        })
+
+    func()
+    return jsonify({
+        'status'    : 200,
+        'message'   : 'Server shutting down...'
+    })
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(app.config['TERM_INPUT'], GPIO.IN)
+    app.run(host=app.config['HOST'], debug=app.config['DEBUG'])
