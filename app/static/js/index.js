824c67372b410321a30313d7e91d4c0daf0404f2
Alexander Chaika <manti.by@gmail.com>
2019-02-13 12:37:35 +0300

    Update weather widget

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,4 +2,6 @@
 
     'use strict';
 
+    $.weather.init();
+
 })(jQuery);
--- app/static/js/weather.js
+++ app/static/js/weather.js
@@ -2,35 +2,48 @@
 
     'use strict';
 
-    let directions = [
-        "N", "NNE", "NE", "ENE",
-        "E","ESE", "SE", "SSE",
-        "S", "SSW", "SW", "WSW",
-        "W", "WNW", "NW", "NNW"];
+    class WeatherWidget {
 
-    let deg_to_dir = (deg) => {
-        let index = Math.floor(((deg / 22.5) + 0.5) % 16);
-        return directions[index];
-    };
+        constructor() {
+            this.directions = [
+                "N", "NNE", "NE", "ENE",
+                "E","ESE", "SE", "SSE",
+                "S", "SSW", "SW", "WSW",
+                "W", "WNW", "NW", "NNW"
+            ];
 
-    let get_weather_data = () => {
-        let template = Handlebars.compile($('#t-weather').html());
-        $.getJSON(
-            'https://api.openweathermap.org/data/2.5/weather?q=Minsk,by&units=metric&appid=aec9289a4fe49b1bca7296d08c1e170b',
-            (data) => {
-                $('#weather').html(template({
-                        temp: data['main']['temp'],
-                        wind: data['wind']['speed'],
-                        wind_dir: deg_to_dir(data['wind']['deg']),
-                        pressure: data['main']['pressure'],
-                        icon: 'http://openweathermap.org/img/w/' + data['weather'][0]['icon'] + '.png'
-                    })
-                );
-            }
-        )
-    };
+            this.template = Handlebars.compile(
+                $('#t-weather').html()
+            );
+        }
 
-    get_weather_data();
-    setInterval(get_weather_data, 5 * 60 * 1000);
+        init() {
+            this.update();
+            setInterval(this.update, 5 * 60 * 1000);
+        }
+
+        deg_to_dir (deg) {
+            let index = Math.floor(((deg / 22.5) + 0.5) % 16);
+            return this.directions[index];
+        };
+
+        update() {
+            $.getJSON(
+                'https://api.openweathermap.org/data/2.5/weather?q=Minsk,by&units=metric&appid=aec9289a4fe49b1bca7296d08c1e170b',
+                (data) => {
+                    $('#weather').html(this.template({
+                            temp: data['main']['temp'],
+                            wind: data['wind']['speed'],
+                            wind_dir: this.deg_to_dir(data['wind']['deg']),
+                            pressure: data['main']['pressure'],
+                            icon: 'http://openweathermap.org/img/w/' + data['weather'][0]['icon'] + '.png'
+                        })
+                    );
+                }
+            )
+        };
+    }
+
+    $.weather = new WeatherWidget();
 
 })(jQuery);
