5ef9c1e89be2fc76f3d3cfa126106e041d7a464a
Alexander Chaika <manti.by@gmail.com>
2019-02-27 16:30:27 +0300

    Add service worker, make more autonomous app

---
+++ app/static/js/sw.js
@@ -0,0 +1,81 @@
+const STATIC_CACHE = 'static-cache',
+    APP_RESOURCES = [
+        '/static/manifest.json',
+
+        '/static/css/lib/bootstrap.min.css',
+        '/static/css/index.css',
+
+        '/static/font/open-sans-regular-cyrillic.woff2',
+        '/static/font/open-sans-regular-latin.woff2',
+        '/static/img/favicon.png',
+
+        '/static/js/lib/jquery-3.3.1.min.js',
+        '/static/js/lib/popper-1.14.3.min.js',
+        '/static/js/lib/bootstrap-4.1.3.min.js',
+        '/static/js/lib/chartjs-2.7.3.min.js',
+        '/static/js/lib/handlebars-4.0.12.min.js',
+
+        '/static/js/lib/sensor.js',
+        '/static/js/widget/plants-latest.js',
+        '/static/js/widget/plants-report.js',
+        '/static/js/widget/weather.js',
+        '/static/js/index.js',
+    ];
+
+
+// Cache static resources
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(STATIC_CACHE).then((cache) => {
+            return cache.addAll(APP_RESOURCES);
+        })
+    );
+});
+
+
+// Clean all other caches, except of static
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cache_names) => {
+            return Promise.all(
+                cache_names.map((cache_name) => {
+                    if (cache_name !== STATIC_CACHE) {
+                        return caches.delete(cache_name);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+// Check all backend calls
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request.clone())
+                    .then((response) => {
+                        let is_failed = !response || response.status !== 200 || response.type !== 'basic',
+                            is_api_call = response.url.indexOf('api') >= 0;
+
+                        // Do not cache failed requests and api calls
+                        if (is_failed || is_api_call) {
+                            return response;
+                        }
+
+                        // Cache app resources
+                        caches.open(STATIC_CACHE)
+                            .then((cache) => {
+                                cache.put(event.request, response.clone());
+                            });
+
+                        return response;
+                    });
+            })
+    );
+});
