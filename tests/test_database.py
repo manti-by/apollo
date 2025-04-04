bcbd10fbd971d0e01dbf27d7a49c0f43b4305af0
Alexander Chaika <manti.by@gmail.com>
2025-04-04 18:01:49 +0300

    Remove server

---
+++ tests/test_database.py
@@ -0,0 +1,37 @@
+from datetime import UTC, datetime
+from decimal import Decimal
+
+from apollo.database import get_sensors_data, save_sensors_data, update_sensor_data
+
+
+class TestDatabase:
+    def test_create_and_get_sensors(self, db_connection):
+        temp = Decimal("20.5")
+        sensors = get_sensors_data(connection=db_connection)
+        assert len(sensors) == 0
+
+        for i in range(3):
+            save_sensors_data(connection=db_connection, sensor_id=f"test {i}", temp=temp)
+
+        sensors = get_sensors_data(connection=db_connection)
+        assert len(sensors) == 3
+
+    def test_create_and_update_sensor(self, db_connection):
+        temp = Decimal("20.5")
+        save_sensors_data(connection=db_connection, sensor_id="test", temp=temp)
+        sensors = get_sensors_data(connection=db_connection)
+        assert len(sensors) == 1
+
+        record_id = sensors[0]["id"]
+        update_sensor_data(connection=db_connection, record_id=record_id, temp=temp)
+        sensors = get_sensors_data(connection=db_connection)
+        assert sensors[0]["temp"] == temp
+
+        update_sensor_data(connection=db_connection, record_id=record_id, temp=temp, context={"test": "test"})
+        sensors = get_sensors_data(connection=db_connection)
+        assert sensors[0]["context"] == {"test": "test"}
+        assert not sensors[0]["synced_at"]
+
+        update_sensor_data(connection=db_connection, record_id=record_id, temp=temp, synced_at=datetime.now(UTC))
+        sensors = get_sensors_data(connection=db_connection)
+        assert sensors[0]["synced_at"]
